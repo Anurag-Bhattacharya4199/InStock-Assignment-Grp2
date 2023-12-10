4380705944430206bb12dd274795033a5b0a1355
Madison <madisonjohnson301@gmail.com>
2023-12-10 17:08:18 -0500

    warehouse list component mobile styling

--- src/components/WareHouseList/WareHouseList.js
+++ src/components/WareHouseList/WareHouseList.js
@@ -1,4 +1,7 @@
 import './WareHouseList.scss'
+import { Link } from 'react-router-dom'
+import DeleteButton from '../../assets/icons/delete_outline-24px.svg'
+import EditIcon from '../../assets/icons/edit-24px.svg'
 
 function WareHouseList(props) {
     const { warehouses } = props
@@ -14,22 +17,43 @@ function WareHouseList(props) {
             </div>
 
             {warehouses.map((item) =>
-                <div key={item.id} className='warehouseList__body--container'>
-                    <div className='warehouseList__body--subcontainer'>
-                        <p>WAREHOUSE</p>
-                        <p>{item.warehouse_name}</p>
-                        <p>ADDRESS</p>
-                        <p>{item.address}</p>
-                        <p>{item.city}, {item.country}</p>
+                <div key={item.id} className='warehouseList__body'>
+                    <div className='warehouseList__body--container'>
+                        <div className='warehouseList__body--subcontainer'>
+                            <div className='warehouseList__body--info'>
+                                <h4>WAREHOUSE</h4>
+                                <p className='p-medium'>{item.warehouse_name}</p>
+                            </div>
+                            <div className='warehouseList__body--info'>
+                                <h4>ADDRESS</h4>
+                                <p className='p-medium'>{item.address}</p>
+                                <p className='p-medium'>{item.city}, {item.country}</p>
+                            </div>
+
+                        </div>
+                        <div className='warehouseList__body--subcontainer'>
+                            <div className='warehouseList__body--info'>
+                                <h4>CONTACT NAME</h4>
+                                <p className='p-medium'>{item.contact_name}</p>
+                            </div>
+                            <div className='warehouseList__body--info'>
+                                <h4>CONTACT INFORMATION</h4>
+                                <p className='p-medium'>{item.contact_phone}</p>
+                                <p className='p-medium'>{item.contact_email}</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className='warehouseList__body--subcontainer'>
-                        <p>CONTACT NAME</p>
-                        <p>{item.contact_name}</p>
-                        <p>Contact Information</p>
-                        <p>{item.contact_phone}</p>
-                        <p>{item.contact_email}</p>
+                    <div className='warehouseList__body--icon-container'>
+                        <Link>
+                            <img src={DeleteButton} alt='delete icon' className='warehouseList__Body--delete-icon'></img>
+                        </Link>
+                        <Link>
+                            <img src={EditIcon} alt='edit icon' className='warehouseList__Body--edit-icon'></img>
+                        </Link>
                     </div>
                 </div>
+
+
             )}
 
 

6bd30f284c350738def31cb5438871b02c732924
Tsering Dhondup <tyddhondup88@gmail.com>
2023-12-11 21:23:51 -0500

    Footer Component created!

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import "./Footer.scss";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className="p-small">© InStock Inc. All Rights Reserved.</p>
+    </div>
+  );
+};
+export default Footer;

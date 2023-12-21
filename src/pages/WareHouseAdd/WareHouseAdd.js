7e2b4faa11d04137ac627d5f8bc9d0f2ca754bc8
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2023-12-20 20:51:30 -0500

    Finished Styling of Add Warehouse at all breakpoints

--- src/pages/WareHouseAdd/WareHouseAdd.js
+++ src/pages/WareHouseAdd/WareHouseAdd.js
@@ -5,82 +5,87 @@ import "./WareHouseAdd.scss";
 function WareHouseAdd() {
   return (
     <main className="addWarehouse">
-      <div className="addWarehouse__headers">
-        <Link to="/">
-          <img
-            src={ArroWBack}
-            alt="Go Back"
-            className="warehouseDetails__header-arrowback"
-          />
-        </Link>
-        <h1 className="addWarehouse__headers-title">Add New Warehouse</h1>
-      </div>
+      <section className="addWarehouse__header">
+        <div className="addWarehouse__header-info">
+          <Link to="/">
+            <img
+              src={ArroWBack}
+              alt="Go Back"
+              className="addWarehouse__header-arrowback"
+            />
+          </Link>
+          <h1 className="addWarehouse__header-title">Add New Warehouse</h1>
+        </div>
+      </section>
       <form className="addWarehouse__form">
-        <div className="addWarehouse__form-warehouseDetails">
-          <h2 className="addWarehouse__form-header">Warehouse Details</h2>
-          <article className="addWarehouse__form-warehouseName">
-            <label className="p-medium">Warehouse Name</label>
-            <input
-              className="addWarehouse__form-warehouse_nameInp"
-              placeholder="Warehouse Name"
-            />
-          </article>
-          <article className="addWarehouse__form-streetAddress">
-            <label className="p-medium">Street Address</label>
-            <input
-              className="addWarehouse__form-warehouse_streetaddressInp"
-              placeholder="Street Address"
-            />
-          </article>
-          <article className="addWarehouse__form-city">
-            <label className="p-medium">City</label>
-            <input
-              className="addWarehouse__form-warehouse_cityInp"
-              placeholder="City"
-            />
-          </article>
-          <article className="addWarehouse__form-country">
-            <label className="p-medium">Country</label>
-            <input
-              className="addWarehouse__form-warehouse_countryInp"
-              placeholder="Country"
-            />
-          </article>
+        <div className="addWarehouse__form-mainInfo">
+          <div className="addWarehouse__form-warehouseDetails">
+            <h2 className="addWarehouse__form-header">Warehouse Details</h2>
+            <article className="addWarehouse__form-warehouseName">
+              <label className="p-medium">Warehouse Name</label>
+              <input
+                className="addWarehouse__form-warehouse_nameInp"
+                placeholder="Warehouse Name"
+              />
+            </article>
+            <article className="addWarehouse__form-streetAddress">
+              <label className="p-medium">Street Address</label>
+              <input
+                className="addWarehouse__form-warehouse_streetaddressInp"
+                placeholder="Street Address"
+              />
+            </article>
+            <article className="addWarehouse__form-city">
+              <label className="p-medium">City</label>
+              <input
+                className="addWarehouse__form-warehouse_cityInp"
+                placeholder="City"
+              />
+            </article>
+            <article className="addWarehouse__form-country">
+              <label className="p-medium">Country</label>
+              <input
+                className="addWarehouse__form-warehouse_countryInp"
+                placeholder="Country"
+              />
+            </article>
+          </div>
+          <div className="addWarehouse__form-contactDetails">
+            <h2 className="addWarehouse__form-header">Contact Details</h2>
+            <article className="addWarehouse__form-contactName">
+              <label className="p-medium">Contact Name</label>
+              <input
+                className="addWarehouse__form-warehouse_contactNameInp"
+                placeholder="Contact Name"
+              />
+            </article>
+            <article className="addWarehouse__form-position">
+              <label className="p-medium">Position</label>
+              <input
+                className="addWarehouse__form-warehouse_positionInp"
+                placeholder="position"
+              />
+            </article>
+            <article className="addWarehouse__form-phoneNum">
+              <label className="p-medium">Phone Number</label>
+              <input
+                className="addWarehouse__form-warehouse_phonenumInp"
+                placeholder="Phone Number"
+              />
+            </article>
+            <article className="addWarehouse__form-email">
+              <label className="p-medium">Email</label>
+              <input
+                className="addWarehouse__form-warehouse_emailInp"
+                placeholder="Email"
+              />
+            </article>
+          </div>
         </div>
-        <div className="addWarehouse__form-contactDetails">
-          <h2 className="addWarehouse__form-header">Contact Details</h2>
-          <article className="addWarehouse__form-contactName">
-            <label className="p-medium">Contact Name</label>
-            <input
-              className="addWarehouse__form-warehouse_contactNameInp"
-              placeholder="Contact Name"
-            />
-          </article>
-          <article className="addWarehouse__form-position">
-            <label className="p-medium">Position</label>
-            <input
-              className="addWarehouse__form-warehouse_positionInp"
-              placeholder="position"
-            />
-          </article>
-          <article className="addWarehouse__form-phoneNum">
-            <label className="p-medium">Phone Number</label>
-            <input
-              className="addWarehouse__form-warehouse_cityInp"
-              placeholder="City"
-            />
-          </article>
-          <article className="addWarehouse__form-email">
-            <label className="p-medium">Email</label>
-            <input
-              className="addWarehouse__form-warehouse_emailInp"
-              placeholder="Email"
-            />
-          </article>
-        </div>
+
         <div className="addWarehouse__form-buttons">
-          <button>Cancel</button>
-          <button>Add WareHouse</button>
+          <button className="addWarehouse__form-cancelBtn">Cancel</button>
+          <button className="addWarehouse__form-addBtn">+ Add WareHouse</button>
         </div>
       </form>
     </main>

1a9a7de38f71951dd9a8ac5369e387bac4b43d0c
Bryce Borer <bryce.borer@gmail.com>
2023-12-22 10:42:16 -0500

    first commit on this branch (need to switch branches)

---
+++ src/components/TitleHeader/TitleHeader.js
@@ -0,0 +1,44 @@
+import './TitleHeader.scss'
+import { Link } from 'react-router-dom';
+import ArroWBack from "../../assets/icons/arrow_back-24px.svg";
+import EditButton from "../../assets/icons/edit-24px.svg";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+// This header can be used for the WareHouseList component and the WareHouseInventoryList component
+function TitleHeader(props) {
+
+    
+
+    return (
+        <section className="header">
+          <div className="header--info">
+            <Link to={`{}`}>
+              <img
+                src={ArroWBack}
+                alt="Go Back"
+                className="header--arrowback"
+              />
+            </Link>
+            <h1 className="header--title">
+              {warehouse.warehouse_name}
+            </h1>
+          </div>
+          <Link
+            to={`/warehouses/:id/edit`}
+            className="header--edit"
+          >
+            <img
+              src={EditButton}
+              className="header--editImg"
+              alt="Edit Warehouse"
+            />
+            <span className="header--editTxt">Edit</span>
+          </Link>
+        </section>
+    )
+}
+
+export default TitleHeader
--- src/pages/WareHouseDetailPage/WareHouseDetailPage.js
+++ src/pages/WareHouseDetailPage/WareHouseDetailPage.js
@@ -6,6 +6,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import InventoryList from "../../components/InventoryList/InventoryList";
+import "../../styles/partials/_global.scss";
 
 function WareHouseDetailPage(props) {
   let { id } = useParams();

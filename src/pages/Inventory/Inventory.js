257d76468234805608ba5a87389db7eac32f990c
Madison <madisonjohnson301@gmail.com>
2023-12-26 12:58:49 -0500

    display delete modal in icon click

--- src/pages/Inventory/Inventory.js
+++ src/pages/Inventory/Inventory.js
@@ -3,12 +3,48 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import InventoryList from "../../components/InventoryList/InventoryList";
 import SearchHeader from "../../components/SearchHeader/SearchHeader";
+import DeleteModal from "../../components/DeleteModal/DeleteModal";
 import axios from "axios";
 
 const Inventory = () => {
   const [hasLoaded, setHasLoaded] = useState(false);
   const [inventoryList, setInventoryList] = useState([]);
+  const [showDeletePopup, setShowDeletePopup] = useState(false);
+  const [deleteInventoryID, setDeleteInventoryID] = useState(null);
+  const [inventoryToDelete, setInventorToDelete] = useState(null);
+  const { id } = useParams();
+
   const API_BASE_URL = "http://localhost:8080";
+
+
+  const handleDeleteClick = (id, item_name) => {
+    setShowDeletePopup(true);
+    setDeleteInventoryID(String(id));
+    setInventorToDelete(item_name.toString());
+    //console.log(id)
+  };
+
+  const handleDeleteConfirmation = () => {
+    // Make a DELETE request to delete the warehouse
+    axios.delete(`${API_BASE_URL}/inventories/${deleteInventoryID}`)
+      .then(() => {
+        console.log(`Successfully deleted inventory item with ID: ${deleteInventoryID}`);
+        // Now, make another DELETE request to delete the warehouse inventory
+        setShowDeletePopup(false);
+        setDeleteInventoryID(null);
+
+      })
+      .catch((error) => {
+        console.error(`Error deleting inventory item: ${error}`);
+
+      });
+  };
+
+  // to  close delete component using cancel or X
+  const handleCloseDeleteComponent = () => {
+    setShowDeletePopup(false);
+    setDeleteInventoryID(null);
+  };
 
   useEffect(() => {
     axios
@@ -21,16 +57,32 @@ const Inventory = () => {
         console.log(err);
       });
     setHasLoaded(true);
-    
+
   }, []);
 
   if (!hasLoaded) {
     return null;
   } else {
+
+
     return (
-      <section>
+      <section className="inventory-list">
         <SearchHeader title="Inventory" addNewItem="Item" />
-        <InventoryList inventoryList={inventoryList} />
+
+        {showDeletePopup && (
+          <div className="overlay">
+            <div className="delete-popup">
+              <DeleteModal
+                inventoryName={inventoryToDelete}
+                itemType="inventory"
+                handleCloseDeleteComponent={handleCloseDeleteComponent}
+                handleDeleteConfirmation={handleDeleteConfirmation}
+              />
+            </div>
+          </div>
+        )}
+
+        <InventoryList inventoryList={inventoryList} onDeleteClick={handleDeleteClick} />
       </section>
     );
   }

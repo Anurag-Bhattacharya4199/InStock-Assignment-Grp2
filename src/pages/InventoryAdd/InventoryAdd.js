c41a1489d6841790d35c7d03f42cd59d51269f87
Anurag-Bhattacharya4199 <anuragbtor@gmail.com>
2024-01-01 21:53:35 -0500

    Validation + Functionality done (Outside of Qty Validation)

--- src/pages/InventoryAdd/InventoryAdd.js
+++ src/pages/InventoryAdd/InventoryAdd.js
@@ -1,23 +1,163 @@
-import { Link, useNavigate } from "react-router-dom";
-import ArroWBack from "../../assets/icons/arrow_back-24px.svg";
+import { useNavigate } from "react-router-dom";
 import "./InventoryAdd.scss";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import ErrorIcon from "../../assets/icons/error-24px.svg";
+import SearchHeader from "../../components/SearchHeader/SearchHeader";
 
 function InventoryAdd() {
   const API_BASE_URL = "http://localhost:8080/warehouses";
   const [warehouses, setWarehouses] = useState([]);
-  const [inStock, setInstock] = useState(false);
+  const [inStock, setInstock] = useState("true");
 
   const [itemName, setItemName] = useState("");
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("");
+  const [itemStatus, setItemStatus] = useState("In Stock");
   const [qty, setQty] = useState("");
   const [warehouse, setWarehouse] = useState("");
 
   const [error, setError] = useState({
     itemNameError: false,
+    descriptionError: false,
+    categoryError: false,
+    qtyError: false,
+    warehouseError: false,
   });
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    //console.log(itemStatus);
+    if (itemStatus === "In Stock") {
+      setInstock("true");
+    } else {
+      setInstock("false");
+    }
+  }, [itemStatus]);
+
+  const postWarehouse = async (
+    warehouseIDVal,
+    itemNameVal,
+    descriptionVal,
+    categoryVal,
+    statusVal,
+    qtyVal
+  ) => {
+    const newItem = {
+      warehouse_id: warehouseIDVal,
+      item_name: itemNameVal,
+      description: descriptionVal,
+      category: categoryVal,
+      status: statusVal,
+      quantity: qtyVal,
+    };
+    console.log(newItem);
+    try {
+      axios.post("http://localhost:8080/inventories", newItem, {
+        "Content-Type": "application/json",
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChangeItemName = (event) => {
+    const itemName = event.target.value;
+    setItemName(itemName);
+  };
+
+  const handleChangeDescription = (event) => {
+    const description = event.target.value;
+    setDescription(description);
+  };
+
+  const handleChangeCategory = (event) => {
+    const category = event.target.value;
+    setCategory(category);
+  };
+
+  const handleChangeQty = (event) => {
+    const qty = event.target.value;
+    setQty(qty);
+  };
+
+  const handleChangeWarehouse = (event) => {
+    const warehouse = event.target.value;
+    setWarehouse(warehouse);
+  };
+
+  const isFormValid = () => {
+    let formComplete = true;
+
+    let errorState = {
+      itemNameError: false,
+      descriptionError: false,
+      categoryError: false,
+      qtyError: false,
+      warehouseError: false,
+    };
+
+    if (itemName.length === 0) {
+      errorState.itemNameError = true;
+      formComplete = false;
+    }
+
+    if (description.length === 0) {
+      errorState.descriptionError = true;
+      formComplete = false;
+    }
+
+    if (category.length === 0) {
+      errorState.categoryError = true;
+      formComplete = false;
+    }
+
+    //Qty Validation to be worked on
+
+    if (warehouse.length === 0) {
+      errorState.warehouseError = true;
+      formComplete = false;
+    }
+
+    setError(errorState);
+
+    return formComplete;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (isFormValid()) {
+      setItemName("");
+      setDescription("");
+      setCategory("Please Select");
+      setQty("");
+      setWarehouse("Please Select");
+      let warehouseId = warehouses.filter((wh) => {
+        return wh.warehouse_name === warehouse;
+      })[0].id;
+      //console.log(warehouseId);
+      //console.log(qty);
+      postWarehouse(
+        warehouseId,
+        itemName,
+        description,
+        category,
+        itemStatus,
+        qty
+      );
+      alert("New Item added");
+      navigate("/inventories");
+    } else {
+      return;
+    }
+  };
+
+  const handleCancel = () => {
+    alert("Item Add Cancelled");
+    navigate("/inventories");
+  };
 
   const fetchWarehouseList = () => {
     axios
@@ -35,52 +175,76 @@ function InventoryAdd() {
     fetchWarehouseList();
   }, []);
 
-  const handleInStockAvailabilityFalse = () => {
-    setInstock(false);
-  };
-
-  const handleInStockAvailabilityTrue = () => {
-    setInstock(true);
-  };
-
   return (
     <main className="addInventoryItem">
-      <section className="addInventoryItem__header">
-        <div className="addInventoryItem__header-info">
-          <Link to="/inventories">
-            <img
-              src={ArroWBack}
-              alt="Go Back"
-              className="addInventoryItem__header-arrowback"
-            />
-          </Link>
-          <h1 className="addWarehouse__header-title">Add New Inventory Item</h1>
-        </div>
-      </section>
-      <form className="addInventoryItem__form">
+      <SearchHeader title="Add Inventory Item" />
+      <form className="addInventoryItem__form" onSubmit={handleSubmit}>
         <div className="addInventoryItem__form-mainInfo">
           <div className="addInventoryItem__form-itemDetails">
             <h2>Item Details</h2>
             <article className="addInventoryItem__form-itemName">
               <label className="p-medium">Item Name</label>
               <input
+                className={`addInventoryItem__form-itemNameInp ${
+                  error.itemNameError
+                    ? "addInventoryItem__form-invalidInput"
+                    : ""
+                }`}
                 placeholder="Item Name"
-                className="addInventoryItem__form-itemNameInp"
+                name="itemName"
+                htmlFor="itemName"
+                value={itemName}
+                onChange={handleChangeItemName}
               />
+              <span
+                className={`addInventoryItem__form-errorMsg ${
+                  error.itemNameError
+                    ? "addInventoryItem__form-errorMsgInvalidInput"
+                    : ""
+                }`}
+              >
+                <img src={ErrorIcon} alt="Error Icon" />
+                This field is required
+              </span>
             </article>
             <article className="addInventoryItem__form-description">
               <label className="p-medium">Description</label>
               <textarea
+                className={`addInventoryItem__form-itemNameInp ${
+                  error.descriptionError
+                    ? "addInventoryItem__form-invalidInput"
+                    : ""
+                }`}
                 placeholder="Please enter a brief item description..."
-                className="addInventoryItem__form-descriptionInp"
+                name="description"
+                htmlFor="description"
+                value={description}
+                onChange={handleChangeDescription}
               ></textarea>
+              <span
+                className={`addInventoryItem__form-errorMsg ${
+                  error.itemNameError
+                    ? "addInventoryItem__form-errorMsgInvalidInput"
+                    : ""
+                }`}
+              >
+                <img src={ErrorIcon} alt="Error Icon" />
+                This field is required
+              </span>
             </article>
             <article className="addInventoryItem__form-category">
               <label className="p-medium">Category</label>
               <select
-                className="addInventoryItem__form-categoryOptions"
-                placeholder="Please select"
-                defaultValue="Please select"
+                className={`addInventoryItem__form-categoryOptions ${
+                  error.categoryError
+                    ? "addInventoryItem__form-invalidInput"
+                    : ""
+                }`}
+                placeholder="Please Select"
+                name="category"
+                htmlFor="category"
+                value={category}
+                onChange={handleChangeCategory}
               >
                 <option value="Please Select">Please Select</option>
                 <option value="Electronics">Electronics</option>
@@ -89,6 +253,16 @@ function InventoryAdd() {
                 <option value="Health">Health</option>
                 <option value="Gear">Gear</option>
               </select>
+              <span
+                className={`addInventoryItem__form-errorMsg ${
+                  error.itemNameError
+                    ? "addInventoryItem__form-errorMsgInvalidInput"
+                    : ""
+                }`}
+              >
+                <img src={ErrorIcon} alt="Error Icon" />
+                This field is required
+              </span>
             </article>
           </div>
           <div className="addInventoryItem__form-itemAvailability">
@@ -102,7 +276,10 @@ function InventoryAdd() {
                     className="addInventoryItem__form-inStockInp"
                     id="In Stock"
                     name="status"
-                    onClick={handleInStockAvailabilityTrue}
+                    htmlFor="status"
+                    value="In Stock"
+                    defaultChecked
+                    onClick={() => setItemStatus("In Stock")}
                   />
                   In stock
                 </label>
@@ -112,38 +289,77 @@ function InventoryAdd() {
                     className="addInventoryItem__form-outofStockInp"
                     id="Out of Stock"
                     name="status"
-                    onClick={handleInStockAvailabilityFalse}
+                    htmlFor="status"
+                    value="Out of Stock"
+                    onClick={() => setItemStatus("")}
                   />
                   Out of stock
                 </label>
               </span>
             </article>
-            <article className="addInventoryItem__form-qty">
+            <article className={`addInventoryItem__form-qty ${inStock}`}>
               <label className="p-medium">Quantity</label>
               <input
-                placeholder="0"
                 className={`addInventoryItem__form-qtyInput ${
-                  inStock ? "" : "addInventoryItem__form-displayNone"
+                  error.categoryError
+                    ? "addInventoryItem__form-invalidInput"
+                    : ""
                 }`}
+                placeholder="0"
+                name="qty"
+                htmlFor="qty"
+                value={qty}
+                onChange={handleChangeQty}
               />
+              <span
+                className={`addInventoryItem__form-errorMsg ${
+                  error.qtyError
+                    ? "addInventoryItem__form-errorMsgInvalidInput"
+                    : ""
+                }`}
+              >
+                <img src={ErrorIcon} alt="Error Icon" />
+                This field is required
+              </span>
             </article>
             <article className="addInventoryItem__form-warehouse">
               <label className="p-medium">WareHouse</label>
               <select
-                className="addInventoryItem__form-warehouseOptions"
+                className={`addInventoryItem__form-warehouseOptions ${
+                  error.warehouseError
+                    ? "addInventoryItem__form-invalidInput"
+                    : ""
+                }`}
                 placeholder="Please select"
                 id="warehouseOptions"
+                defaultValue="Please select"
+                onChange={handleChangeWarehouse}
               >
                 <option value="Please Select">Please Select</option>
                 {warehouses.map((item) => {
                   return <option key={item.id}>{item.warehouse_name}</option>;
                 })}
               </select>
+              <span
+                className={`addInventoryItem__form-errorMsg ${
+                  error.warehouseError
+                    ? "addInventoryItem__form-errorMsgInvalidInput"
+                    : ""
+                }`}
+              >
+                <img src={ErrorIcon} alt="Error Icon" />
+                This field is required
+              </span>
             </article>
           </div>
         </div>
         <div className="addInventoryItem__form-buttons">
-          <button className="addInventoryItem__form-cancelBtn">Cancel</button>
+          <button
+            className="addInventoryItem__form-cancelBtn"
+            onClick={handleCancel}
+          >
+            Cancel
+          </button>
           <button className="addInventoryItem__form-addBtn">+ Add Item</button>
         </div>
       </form>
